a64893c346e3458a634e977b06692d974230a04e
Serg8787 <tsybulnikserg@gmail.com>
2022-11-18 09:56:19 +0100

    delete unnecessary files

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-import 'package:info_device/info_device.dart';
 import 'package:info_device/device_information.dart';
 
 void main() {
--- lib/info_device.dart
+++
@@ -1,8 +0,0 @@
-
-import 'info_device_platform_interface.dart';
-
-class InfoDevice {
-  Future<String?> getPlatformVersion() {
-    return InfoDevicePlatform.instance.getInfoDevice();
-  }
-}
